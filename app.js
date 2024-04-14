9c14b8b65d3a31508d32a8e36f09e23fb62eb84b
Bkdel <bkdl26@icloud.com>
2024-04-14 22:54:25 +0100

    api needs merged, week10lab implemented

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 5500;
-
+const axios = require("axios");
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
@@ -30,27 +31,55 @@ const browser_lang = (req, res, next) => {
 
 // routes
 
+
+// the titles (except 404) are not connected to anything, so may be worthwhile changing them all)
 app.get('/', log_data, browser_lang, (req, res) => {
-    res.render("index", { title: 'home' })
+    res.render("index", { title: 'Pokécard Palace' })
 });
 
-// use remainder of code below as a baseline for project, delete and reconfigure if needed
-// bsically a guide for the next pages to make
-
-// maybe better being account? adjust when made
 app.get('/signup', log_data, browser_lang, (req, res) => {
-    res.render("signup", { title: 'sign-up' })
+    res.render("signup")
 });
 
 app.get('/contact', log_data, browser_lang, (req, res) => {
-    res.render("contact", { title: 'contact us' })
+    res.render("contact")
 });
 
-// route for contact form post? Do I need this? 
-app.post('/contact', (req, res) => {
-    res.redirect('/');
+
+app.get('/collections', (req, res) => {
+
+    let ep = `http://localhost:4000/collections/`;
+
+    axios.get(ep).then((response) => {
+        let cdata = response.data;
+        res.render('collections', { title: 'Collections', cdata });
+
+    });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.get('/card', (req, res) => {
+
+    let item_id = req.query.details;
+    let endp = `http://localhost:4000/collections/${item_id}`;
+
+    axios.get(endp).then((response) => {
+        res.send(response.data);
+    });
+
+});
+
+
+// 404 Route: Must be last route here
+app.get('*', log_data, browser_lang, (req, res) => {
+    res.render("404", { title: '404 Error' })
+});
+
+
+// i defined an if else in this one to handle errors here 
+app.listen(PORT, (err) => {
+    if (err) {
+        console.error('Error occurred while starting the server:', err);
+    } else {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    }
 });
